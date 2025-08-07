347e3434bef95c9b17b72e077fae0e63700a64f4
Thasheeee <thashee2003@gmail.com>
2025-08-07 14:24:36 +0530

    created the navbar

---
+++ netflix/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search_icon.svg";
+import bellIcon from "../../assets/bell_icon.svg";
+import profileIcon from "../../assets/profile_img.png";
+import dropdown from "../../assets/caret_icon.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={logo} alt=""></img>
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={searchIcon} alt="" className="icons"></img>
+        <p>Children</p>
+        <img src={bellIcon} alt="" className="icons"></img>
+        <div className="navbar-profile">
+          <img src={profileIcon} alt="" className="profile"></img>
+          <img src={dropdown} alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ netflix/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import NavrBar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+
+    <NavrBar/>
+
+    </div>
+  )
+}
+
+export default Home
