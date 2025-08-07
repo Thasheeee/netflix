de5a72f5e855062fd70005a8367c2c4d662b8031
Thasheeee <thashee2003@gmail.com>
2025-08-07 15:51:16 +0530

    cover page added

--- netflix/src/pages/Home/Home.jsx
+++ netflix/src/pages/Home/Home.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import './Home.css'
-import NavrBar from '../../components/Navbar/Navbar'
+import React from "react";
+import "./Home.css";
+import NavrBar from "../../components/Navbar/Navbar";
+import heroBanner from "../../assets/hero_banner.jpg";
+import heroTitle from "../../assets/hero_title.png";
+import playIcon from "../../assets/play_icon.png";
+import infoIcon from "../../assets/info_icon.png";
 
 const Home = () => {
   return (
-    <div className='home'>
-
-    <NavrBar/>
-
+    <div className="home">
+      <NavrBar />
+      <div className="hero">
+        <img src={heroBanner} alt="" className="banner-img"></img>
+        <div className="hero-caption">
+          <img src={heroTitle} alt="" className="caption-img"></img>
+          <p>
+            Dsicovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn"><img src={playIcon} alt=""></img>Play</button>
+            <button className="btn dark-btn"><img src={infoIcon} alt=""></img>More info</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
